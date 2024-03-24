4455550a0079cae4f01aa7a488c4a77de0383178
Kritesh12-slack <148207048+Kritesh12-slack@users.noreply.github.com>
2024-03-24 01:42:37 +0000

    i/o & boilerplate done

---
+++ C++ Basics/vid1.cpp
@@ -0,0 +1,41 @@
+// // #include<iostream>
+// #include<bits/stdc++.h> // by this u can include every single library of c++
+// using namespace std;
+
+// int main(){
+
+
+//     // std::cout<< "Hello World" << "\n"; // Faster
+//     // std:: cout<<"Hello World" << std::endl;  // Slower as compared to '\n'
+//     // std::cout<< "Hello World";
+
+//     // we can use namespace decleration to avoid using std everywhere
+
+//     // cout<<"Hello world" << '\n' << "without std ;)";
+
+//     int x,y;
+//     cin >> x >> y;
+//     cout << "Value of x :" << x << '\n' << "Value of y :" << y;
+//     return 0;
+// }
+
+
+// ------------------------------------------------------------------------------------------------------- 
+
+
+// Boiler Plate Code of C++
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+
+//     return 0;
+// }
+
+// --------------------------------------------------------------------------------------------------
+
+// Datatypes 
+
+
+
