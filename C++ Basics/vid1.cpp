d07abcbaa4d947b9a097f115329f9aae507b83a1
Kritesh12-slack <148207048+Kritesh12-slack@users.noreply.github.com>
2024-03-24 04:42:52 +0000

    switch statement done

--- C++ Basics/vid1.cpp
+++ C++ Basics/vid1.cpp
@@ -166,7 +166,48 @@
 
 // ------------------------------------------------------------------------------------------------
 
+// Switch Statement
 
+// Problem 
+// Take a number between 1-7 and print the corresponding day if the input is invalid say Invalid input
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int dayNum;
+    cout<<"Enter the Number : ";
+    cin>>dayNum;
+    switch (dayNum)
+    {
+    case 1:
+        cout<<"Sunday";
+        break;
+    case 2:
+        cout<<"Monday";
+        break;
+    case 3:
+        cout<<"Tuesday";
+        break;
+    case 4:
+        cout<<"Wednesday";
+        break;
+    case 5:
+        cout<<"Thusday";
+        break;
+    case 6:
+        cout<<"Friday";
+        break;
+    case 7:
+        cout<<"Saturday";
+        break;
+    
+    default:
+        cout<<"Invalid Input";
+        break;
+    }
+    return 0;
+}
 
 
 
